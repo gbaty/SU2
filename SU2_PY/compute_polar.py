05c7dc11b4e0853fd4725e5b6f3ab8afa72ec466
Guillaume Bâty <guillaume.baty@gmx.fr>
2016-03-10 14:46:36 +0100

    Support both Python 2.6+ and Python 3.4+
    Main changes from python 2 to python 3 are:
    'raise Exception "message"'  ->  'raise Exception("message")'
    print "message"  -> print("message")
    long, int  ->  int
    raw_input  -> input
    cPickle  ->  pickle
    
    input  -> eval(input()) # not used in SU2, just for information
    
    This commit also removes unused import, especially import of os, shutil and copy
    
    Useful links:
      - https://docs.python.org/3/howto/pyporting.html
      - http://python3porting.com
      - https://docs.python.org/3/whatsnew/3.0.html#library-changes, 4th item.

--- SU2_PY/compute_polar.py
+++ SU2_PY/compute_polar.py
@@ -29,6 +29,9 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with SU2. If not, see <http://www.gnu.org/licenses/>.
 
+# make print(*args) function available in PY2.6+, does'nt work on PY < 2.6
+from __future__ import print_function
+
 # imports
 import numpy as np
 from optparse import OptionParser
@@ -268,14 +271,13 @@ def main():
          # --------- end of dummy optimization variables definition section -----------------------
          #
          
-    
          # set angle of attack and side-slip angle
          konfig.AOA = AngleAttack
          konfig.SIDESLIP_ANGLE = SIDESLIP_ANGLE
          konfig.MACH_NUMBER = MachNumber
          caseName='DIRECT_M_'+str(MachNumber)+'_AOA_'+str(AngleAttack)
-         print 'Mach = ' , konfig.MACH_NUMBER , 'AOA = ' , konfig.AOA
-         print 'case :'+caseName
+         print('Mach = ' , konfig.MACH_NUMBER , 'AOA = ' , konfig.AOA)
+         print('case :'+caseName)
     
          # run su2
          if options.Wind:
