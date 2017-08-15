c744598a5a5ff5e5bf18b7a5242770ea98ffb5d6
Pete Bachant <petebachant@gmail.com>
2017-08-14 21:39:42 -0400

    Send stderr to test case log files

--- TestCases/TestCase.py
+++ TestCases/TestCase.py
@@ -82,7 +82,7 @@ class TestCase:
 
         # Assemble the shell command to run SU2
         logfilename = '%s.log' % os.path.splitext(self.cfg_file)[0]
-        command = "%s %s > %s" % (self.su2_exec, self.cfg_file,logfilename)
+        command = "%s %s > %s 2>&1" % (self.su2_exec, self.cfg_file, logfilename)
 
         # Run SU2
         workdir = os.getcwd()
@@ -207,7 +207,7 @@ class TestCase:
 
         # Assemble the shell command to run
         logfilename = '%s.log' % os.path.splitext(self.cfg_file)[0]
-        command = "%s -f %s > %s" % (self.su2_exec, self.cfg_file, logfilename)
+        command = "%s -f %s > %s 2>&1" % (self.su2_exec, self.cfg_file, logfilename)
 
         # Run SU2
         workdir = os.getcwd()
@@ -274,7 +274,7 @@ class TestCase:
 
         # Assemble the shell command to run SU2
         logfilename = '%s.log' % os.path.splitext(self.cfg_file)[0]
-        command = "%s %s > %s" % (self.su2_exec, self.cfg_file,logfilename)
+        command = "%s %s > %s 2>&1" % (self.su2_exec, self.cfg_file, logfilename)
 
         # Run SU2
         workdir = os.getcwd()
@@ -395,7 +395,7 @@ class TestCase:
 
         # Assemble the shell command to run SU2
         logfilename = '%s.log' % os.path.splitext(self.cfg_file)[0]
-        command = "%s %s > %s" % (self.su2_exec, self.cfg_file,logfilename)
+        command = "%s %s > %s 2>&1" % (self.su2_exec, self.cfg_file, logfilename)
 
         # Run SU2
         workdir = os.getcwd()
@@ -522,7 +522,7 @@ class TestCase:
     
         # Assemble the shell command to run SU2
         logfilename = '%s.log' % os.path.splitext(self.cfg_file)[0]
-        command = "%s %s > %s" % (self.su2_exec, self.cfg_file,logfilename)
+        command = "%s %s > %s 2>&1" % (self.su2_exec, self.cfg_file, logfilename)
     
         # Run SU2
         workdir = os.getcwd()
